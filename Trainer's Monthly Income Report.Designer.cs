7c50304298b27da4ed9d78543ae81259ce7fcd16
Wong123456 <33374528+Wong123456@users.noreply.github.com>
2023-06-15 12:26:41 +0800

    Add files via upload
    Upload Admin's file

---
+++ Trainer's Monthly Income Report.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Assignment_Admin_
+{
+    partial class Trainer_s_Monthly_Income_Report
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbTrainerID = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLevel = new System.Windows.Forms.TextBox();
+            this.txtIncome = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lstModules = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Trainer ID";
+            // 
+            // cmbTrainerID
+            // 
+            this.cmbTrainerID.FormattingEnabled = true;
+            this.cmbTrainerID.Location = new System.Drawing.Point(88, 83);
+            this.cmbTrainerID.Name = "cmbTrainerID";
+            this.cmbTrainerID.Size = new System.Drawing.Size(186, 24);
+            this.cmbTrainerID.TabIndex = 1;
+            this.cmbTrainerID.SelectedIndexChanged += new System.EventHandler(this.cmbTrainerID_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(85, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Level";
+            // 
+            // txtLevel
+            // 
+            this.txtLevel.Location = new System.Drawing.Point(88, 172);
+            this.txtLevel.Name = "txtLevel";
+            this.txtLevel.Size = new System.Drawing.Size(186, 22);
+            this.txtLevel.TabIndex = 3;
+            // 
+            // txtIncome
+            // 
+            this.txtIncome.Location = new System.Drawing.Point(88, 250);
+            this.txtIncome.Name = "txtIncome";
+            this.txtIncome.Size = new System.Drawing.Size(186, 22);
+            this.txtIncome.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(88, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Monthly Income";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(398, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 16);
+            this.label4.TabIndex = 6;
+            // 
+            // lstModules
+            // 
+            this.lstModules.FormattingEnabled = true;
+            this.lstModules.ItemHeight = 16;
+            this.lstModules.Location = new System.Drawing.Point(401, 83);
+            this.lstModules.Name = "lstModules";
+            this.lstModules.Size = new System.Drawing.Size(177, 180);
+            this.lstModules.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(401, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(122, 16);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Module\'s Assigned";
+            // 
+            // btnHome
+            // 
+            this.btnHome.Location = new System.Drawing.Point(-1, -2);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(64, 23);
+            this.btnHome.TabIndex = 11;
+            this.btnHome.Text = "Home";
+            this.btnHome.UseVisualStyleBackColor = true;
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            // 
+            // Trainer_s_Monthly_Income_Report
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 396);
+            this.Controls.Add(this.btnHome);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lstModules);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtIncome);
+            this.Controls.Add(this.txtLevel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbTrainerID);
+            this.Controls.Add(this.label1);
+            this.Name = "Trainer_s_Monthly_Income_Report";
+            this.Text = "Trainer\'s Monthly Income Report";
+            this.Load += new System.EventHandler(this.Trainer_s_Monthly_Income_Report_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbTrainerID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLevel;
+        private System.Windows.Forms.TextBox txtIncome;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox lstModules;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnHome;
+    }
+}
