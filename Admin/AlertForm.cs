8cc929d77ab8acdfb00f7d59a5a7493cef7dbaec
Wong123456 <wong.weixiang181103200302@gmail.com>
2023-06-16 10:31:54 +0800

    Created class trainer and alert form(not working)

---
+++ Admin/AlertForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Asssignment.Admin
+{
+    public partial class AlertForm : Form
+    {
+
+        private bool Confirm;
+
+        public bool confirm
+        {
+            get { return Confirm; }
+            set { Confirm = value; }
+        }
+
+        public AlertForm()
+        {
+            InitializeComponent();
+        }
+
+        public void Delete()
+        {
+            lblAlertText.Text = "You are trying to delete a trainer! Press Confirm if you want to continue.";
+            InitializeComponent();
+        }
+
+        private void btnConfirm_Click(object sender, EventArgs e)
+        {
+            confirm = true;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            confirm = false;
+        }
+    }
+}
